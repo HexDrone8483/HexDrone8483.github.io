1a3f519acca609e2cccfaa56b6fb8804b5e78fd7
HexDrone8483 <HexDrone8483@gmail.com>
2022-01-02 18:06:47 +0000

    Amended repo history.

---
+++ mobilemenu.js
@@ -0,0 +1,8 @@
+function hamburgerMenu() {
+  var x = document.getElementById("nav-bar");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
